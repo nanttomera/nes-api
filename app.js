8f4832f1bf8414732563166fb6dd53b6824b8f66
Radoslaw Gorecki <nanttomera@gmail.com>
2021-03-01 22:18:01 +0100

    .

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ passport.deserializeUser(function (obj, done) {
   done(null, obj);
 });
 
-const STEAM_API_KEY = "5A7156714F8A3DC54F0C8582AC5C05E4x";
+const STEAM_API_KEY = "5A7156714F8A3DC54F0C8582AC5C05E4";
 passport.use(
   new SteamStrategy(
     {
